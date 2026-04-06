fcff3d75284bde010ce5f315d04c55fb017ee349
RaghavNagi <raghavnagi81@gmail.com>
2026-04-07 02:14:52 +0530

    setup for notepad Notepad_Project

---
+++ 06_Dynamic_Routing_Form_param/script.js
@@ -0,0 +1,16 @@
+// initialize a project with npm
+// express install
+
+
+// setting up parser for forms
+
+// setting up ejs for ejs pages
+    // -> install ejs from npm
+    // -> setup ejs as a view engine
+
+// setting up public static files
+
+
+// dynamic routing
+    // -> dynamic routing
+    // -> how to get data coming from frontend at backend routing
