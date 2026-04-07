8a91b6a738604853278eab010e62a0737f630add
RaghavNagi <raghavnagi81@gmail.com>
2026-04-08 01:58:00 +0530

    note taking project

--- Notepad_Project/index.js
+++ Notepad_Project/index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const app = express()
 const path = require('path')
+const fs = require('fs')
 
 app.use(express.json())
 app.use(express.urlencoded({extended: true}))
@@ -8,14 +9,27 @@ app.use(express.static(path.join("__dirname","public")))
 app.set("view engine", "ejs")
 
 app.get("/",function(req,res){
-    res.render("index.ejs")
+    fs.readdir(`./tasks`, function(err, tasks){
+        res.render("index.ejs", {tasks: tasks})
+    })
 })
-
-app.get("/profile/:username",function(req,res){
-    res.send(`welcome, ${req.params.username}`)
+app.post("/create",function(req,res){
+    fs.writeFile(`./tasks/${req.body.title.split(" ").join("")}.txt`,req.body.details, function(err){
+        res.redirect("/")
+    })
 })
-app.get("/profile/:username/:age",function(req,res){
-    res.send(`welcome, ${req.params.username} of age ${req.params.age}`)
+app.get("/tasks/:filename",function(req,res){
+    fs.readFile(`./tasks/${req.params.filename}`,"utf-8",function(err, filedata){
+        res.render('show.ejs', {filename: req.params.filename, filedata: filedata})
+    })
+})
+app.get("/edit/:filename",function(req,res){
+    res.render('edit',{filename: req.params.filename})
+})
+app.post("/edit",function(req,res){
+    fs.rename(`./tasks/${req.body.previous}`, `./tasks/${req.body.new}.txt`, function(err){
+        res.redirect("/")
+    })
 })
 
 app.listen(3000, function(){
