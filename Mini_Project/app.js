7f612f87318c100fc00061ef0670fef7ae09524a
RaghavNagi <raghavnagi81@gmail.com>
2026-04-15 00:46:57 +0530

    authentication and authorization using jwt and cookie-parser

--- Mini_Project/app.js
+++ Mini_Project/app.js
@@ -3,7 +3,10 @@ const app = express()
 
 const Path = require('path')
 const userModel = require('./model/user')
+const postModel = require('./model/post')
 const cookieParser = require('cookie-parser')
+const bcrypt = require('bcrypt')
+const jwt = require("jsonwebtoken")
 
 app.use(express.json())
 app.use(express.urlencoded({extended: true}))
@@ -16,9 +19,68 @@ app.get("/",(req,res)=>{
     res.render('index');
 })
 
-app.post("/register", (req,res)=>{
-    
+app.get("/profile",isLoggedIn,(req,res)=>{
+    console.log(req.user);
+    res.render('profile');
 })
+
+
+app.post("/register",async (req,res)=>{
+    
+    let {email, username, password, age, name} = req.body
+
+    let user = await userModel.findOne({email})
+    if (user) return res.status(500).send("user already register");
+
+    bcrypt.genSalt(10, (err,salt)=>{
+        bcrypt.hash(password, salt,async (err,hash)=>{
+            let user = await userModel.Create({
+                username,
+                name,
+                age,
+                email,
+                password: hash
+            })
+
+            let token = jwt.sign({email: email, userid: user._id}, "dotenv_secret_key")
+            res.cookie("token",token)
+            res.send("registered")
+        })
+    })
+})
+
+app.post("/login",async (req,res)=>{
+    
+    let {email, password} = req.body
+
+    let user = await userModel.findOne({email})
+    if (!user) return res.status(500).send("Something went wrong");
+
+    bcrypt.compare(password, user.password, function(err, result){
+        if (result){
+            let token = jwt.sign({email: email, userid:user._id},"dotenv_secret_key")
+            res.cookie("token",token)
+            res.status(200).send("you can login")
+        }
+        else {res.redirect('/login')}
+    })
+})
+
+app.get("/logout",(req,res)=>{
+    res.cookie("token","")
+    res.redirect('/login');
+})
+
+function isLoggedIn(req,res,next){
+    if (req.cookies.token === ""){
+        res.send("You must be logged in")
+    }
+    else{
+        let data = jwt.verify(req.cookies.token,"dotenv_secret_key")
+        req.user = data
+        next()
+    }
+}
 
 
 app.listen(3000)
