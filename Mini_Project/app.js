211d07968ad4088be7df1d7f20f6f207dea6cca4
RaghavNagi <raghavnagi81@gmail.com>
2026-04-15 22:48:40 +0530

    post like and edit feature

--- Mini_Project/app.js
+++ Mini_Project/app.js
@@ -7,6 +7,7 @@ const postModel = require('./model/post')
 const cookieParser = require('cookie-parser')
 const bcrypt = require('bcrypt')
 const jwt = require("jsonwebtoken")
+const user = require('./model/user')
 
 app.use(express.json())
 app.use(express.urlencoded({extended: true}))
@@ -22,6 +23,32 @@ app.get("/",(req,res)=>{
 app.get("/profile",isLoggedIn,async (req,res)=>{
     let user = await userModel.findOne({email: req.user.email}).populate("posts")
     res.render('profile', {user});
+})
+
+app.get("/like/:id",isLoggedIn,async (req,res)=>{
+    let post = await post.findOne({_id: req.params.id}).populate("user")
+
+    if (post.likes.indexOf(req.user.userid) === -1){
+        post.likes.push(req.user.userid)
+    }
+    else{
+        post.likes.splice(post.likes.indexOf(req.user.userid), 1)
+    }
+
+    await post.save()
+    res.redirect('profile');
+})
+
+app.get("/edit/:id",isLoggedIn,async (req,res)=>{
+    let post = await post.findOne({_id: req.params.id}).populate("user")
+
+    res.render('edit', {post})
+})
+
+app.post("/update/:id",isLoggedIn,async (req,res)=>{
+    let post = await post.findOneAndUpdate({_id: req.params.id}, {content: req.body.content}).populate("user")
+
+    res.render('edit', {post})
 })
 
 app.post("/post",isLoggedIn,async (req,res)=>{
