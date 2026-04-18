ae9afdf3c33e3bbb2b7e1d68b0fae8ea99c49982
RaghavNagi <raghavnagi81@gmail.com>
2026-04-18 23:02:36 +0530

    added profile feature

--- Mini_Project/app.js
+++ Mini_Project/app.js
@@ -9,7 +9,7 @@ const cookieParser = require('cookie-parser')
 const bcrypt = require('bcrypt')
 const jwt = require("jsonwebtoken")
 const user = require('./model/user')
-const multer = require('./config/multer')
+const upload = require('./config/multer')
 
 
 app.use(express.json())
@@ -24,6 +24,16 @@ app.get("/",(req,res)=>{
     res.render('index');
 })
 
+app.get("/profile/upload",(req,res)=>{
+    res.render('profileupload');
+})
+
+app.post("/upload", isLoggedIn, upload.signal("image"),async (req,res)=>{
+    let user = await userModel.findOne({email: req.user.email})
+    user.profilepic = req.file.filename
+    await user.save()
+    res.redirect("profile")
+})
 
 app.get("/profile",isLoggedIn,async (req,res)=>{
     let user = await userModel.findOne({email: req.user.email}).populate("posts")
